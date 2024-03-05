27b7a9475b600f442097f7bad504577f365b619d
Hien Tran <79175744+hientran-sudo@users.noreply.github.com>
2024-03-05 15:04:19 -0500

    Create data.py

---
+++ coffee_machine_classes/data.py
@@ -0,0 +1,46 @@
+# data.py
+menu = {
+  "espresso": 
+  {
+    "ingredients":
+    {
+      "water": 50,
+      "coffee": 18      
+    },
+    "cost": 2.5   
+  },
+  "latte":
+  {
+    "ingredients":
+    {
+      "water": 200,
+      "coffee": 24,
+      "milk": 150
+    },
+    "cost": 4.5     
+  },
+  "cappuccino":
+  {
+    "ingredients":
+    {
+      "water": 250,
+      "coffee":24,
+      "milk": 100
+    },
+    "cost": 5.5
+  }
+}
+
+report = {
+  "water": 300,
+  "coffee": 76,
+  "milk": 100,
+  "money": 2.5
+}
+
+coin = {
+  "quarters": 0.25,
+  "dimes": 0.10,
+  "nickles": 0.5,
+  "pennis": 0.01
+}
