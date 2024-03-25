0efb390930a4b5e32d8a77b028ab0d8e329fb716
Hien Tran <79175744+hientran-sudo@users.noreply.github.com>
2024-03-25 17:04:03 -0400

    Update snake.py

--- snake_game_class/snake.py
+++ snake_game_class/snake.py
@@ -27,23 +27,21 @@ class Snake:
         #self.snake[0].right(90)
         self.snake[0].forward(20)
     
-    def turn_right(self):
-        screen.update()
-        time.sleep(0.1)
-        self.snake[0].color("blue")
-        for i in range(len(self.snake)-1,0,-1):
-            self.snake[i].goto(self.snake[i-1].xcor(),self.snake[i-1].ycor())
-        
-        self.snake[0].right(90)
-        self.snake[0].forward(20)
+    def up(self):
+        if self.snake[0].heading() != 270:
+            self.snake[0].setheading(90)
 
-    def turn_left(self):
-        screen.update()
-        time.sleep(0.1)
-        self.snake[0].color("blue")
-        for i in range(len(self.snake)-1,0,-1):
-            self.snake[i].goto(self.snake[i-1].xcor(),self.snake[i-1].ycor())
-        
-        self.snake[0].left(90)
-        self.snake[0].forward(20)    
+
+    def down(self):
+        if self.snake[0].heading() != 90:
+            self.snake[0].setheading(270)
+
+    def left(self):
+        if self.snake[0].heading() != 0:
+            self.snake[0].setheading(180)
+
+    def right(self):
+        if self.snake[0].heading() != 180:
+            self.snake[0].setheading(0)
+   
  
