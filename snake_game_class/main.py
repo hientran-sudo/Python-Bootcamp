7cb2bdc4c75492bcf73c4cedb1ee0f3769561e17
Hien Tran <79175744+hientran-sudo@users.noreply.github.com>
2024-03-26 17:53:05 -0400

    Update main.py

--- snake_game_class/main.py
+++ snake_game_class/main.py
@@ -1,14 +1,19 @@
 from turtle import Screen
 from snake import Snake
+import time
 
 screen = Screen()
 snake = Snake()
   
 screen.listen()
-screen.onkey(key="s", fun=snake.move_forward)
 screen.onkey(key="Up", fun=snake.up)
 screen.onkey(key="Down", fun=snake.down)
 screen.onkey(key="Right", fun=snake.right)
 screen.onkey(key="Left", fun=snake.left)
 
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
 screen.exitonclick()
