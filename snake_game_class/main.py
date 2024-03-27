46625ff03cc16a869cb57b35f2525c5264851d7b
Hien Tran <79175744+hientran-sudo@users.noreply.github.com>
2024-03-27 18:09:29 -0400

    Update main.py

--- snake_game_class/main.py
+++ snake_game_class/main.py
@@ -1,10 +1,11 @@
 from turtle import Screen
 from snake import Snake
-import time
+import time, math
 
 screen = Screen()
 snake = Snake()
-  
+food = Food()
+
 screen.listen()
 screen.onkey(key="Up", fun=snake.up)
 screen.onkey(key="Down", fun=snake.down)
@@ -16,4 +17,11 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
+    dis_x = snake.head_x_cor() - food.food_x_cor()
+    dis_y = snake.head_y_cor() - food.food_y_cor()
+    distance = math.sqrt(dis_x**2 + dis_y**2)
+
+    if distance < 15:
+        print("nom nom")
+      
 screen.exitonclick()
