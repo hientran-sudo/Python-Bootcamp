e4b6cb5ebd329478011ca56a45fe774177a2152c
Hien Tran <79175744+hientran-sudo@users.noreply.github.com>
2024-04-01 18:27:09 -0400

    Update main.py

--- snake_game_class/main.py
+++ snake_game_class/main.py
@@ -1,27 +1,47 @@
-from turtle import Screen
-from snake import Snake
-import time, math
+ from turtle import Screen
+ from snake import Snake
+ from food import Food
+ import time, math, random
 
-screen = Screen()
-snake = Snake()
-food = Food()
+ # screen initalization
+ screen = Screen()
+ screen.tracer(0)
+ score = 0
+ screen.title(f"Snake Game - Score: {score}")
 
-screen.listen()
-screen.onkey(key="Up", fun=snake.up)
-screen.onkey(key="Down", fun=snake.down)
-screen.onkey(key="Right", fun=snake.right)
-screen.onkey(key="Left", fun=snake.left)
+ # objects initalization
+ snake = Snake()
+ food = Food(random.randint(-280,280), random.randint(-280,280))
+ #food.create_food()
 
-game_is_on = True
-while game_is_on:
-    screen.update()
-    time.sleep(0.1)
-    snake.move()
-    dis_x = snake.head_x_cor() - food.food_x_cor()
-    dis_y = snake.head_y_cor() - food.food_y_cor()
-    distance = math.sqrt(dis_x**2 + dis_y**2)
+ # screen listener
+ screen.listen()
+ screen.onkey(key="Up", fun=snake.up)
+ screen.onkey(key="Down", fun=snake.down)
+ screen.onkey(key="Right", fun=snake.right)
+ screen.onkey(key="Left", fun=snake.left)
 
-    if distance < 15:
-        print("nom nom")
+ # game body
+ game_is_on = True
+ while game_is_on:
+     screen.update()
+     time.sleep(0.1)
+
+     snake.move()
+
+     # calculate distance
+     dis_x = snake.head_x_cor() - food.x_cor
+     dis_y = snake.head_y_cor() - food.y_cor
+
+     distance = math.sqrt(dis_x**2 + dis_y**2)
+     #print(distance)
+
+     if distance < 20:
+         print("nom nom")
+         #food.remove_food()
+         snake.grow()
+         food = Food(random.randint(-280,280), random.randint(-280,280))
+         score = score + 1
+         screen.title(f"Snake Game - Score: {score}")
       
 screen.exitonclick()
