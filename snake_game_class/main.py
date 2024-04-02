6efc68d287f287badbeb890f672183c3b20dc883
Hien Tran <79175744+hientran-sudo@users.noreply.github.com>
2024-04-02 19:10:56 -0400

    Update main.py

--- snake_game_class/main.py
+++ snake_game_class/main.py
@@ -1,47 +1,60 @@
- from turtle import Screen
- from snake import Snake
- from food import Food
- import time, math, random
+from turtle import Screen
+from snake import Snake
+from food import Food
+from score import Scoreboard
+import time, math, random
 
- # screen initalization
- screen = Screen()
- screen.tracer(0)
- score = 0
- screen.title(f"Snake Game - Score: {score}")
+# screen initalization
+screen = Screen()
+screen.tracer(0)
+score = 0
+screen.title("Snake Game")
 
- # objects initalization
- snake = Snake()
- food = Food(random.randint(-280,280), random.randint(-280,280))
- #food.create_food()
+# objects initalization
+scoreboard = Scoreboard()
+snake = Snake()
+food = Food()
 
- # screen listener
- screen.listen()
- screen.onkey(key="Up", fun=snake.up)
- screen.onkey(key="Down", fun=snake.down)
- screen.onkey(key="Right", fun=snake.right)
- screen.onkey(key="Left", fun=snake.left)
+# screen listener
+screen.listen()
+screen.onkey(key="Up", fun=snake.up)
+screen.onkey(key="Down", fun=snake.down)
+screen.onkey(key="Right", fun=snake.right)
+screen.onkey(key="Left", fun=snake.left)
 
- # game body
- game_is_on = True
- while game_is_on:
-     screen.update()
-     time.sleep(0.1)
+# game body
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
 
-     snake.move()
+    snake.move()
 
-     # calculate distance
-     dis_x = snake.head_x_cor() - food.x_cor
-     dis_y = snake.head_y_cor() - food.y_cor
+    # calculate distance
+    dis_x = snake.head_x_cor() - food.x_cor
+    dis_y = snake.head_y_cor() - food.y_cor
 
-     distance = math.sqrt(dis_x**2 + dis_y**2)
-     #print(distance)
+    distance = math.sqrt(dis_x**2 + dis_y**2)
+    #print(distance)
 
-     if distance < 20:
-         print("nom nom")
-         #food.remove_food()
-         snake.grow()
-         food = Food(random.randint(-280,280), random.randint(-280,280))
-         score = score + 1
-         screen.title(f"Snake Game - Score: {score}")
-      
+    if distance < 20:
+        print("nom nom")
+        food.remove_food()
+        snake.grow()
+
+        scoreboard.add_score()
+        scoreboard.update_score()
+
+        food = Food()
+
+    # detect if hit the wall
+    if snake.head_x_cor() >= 385 or snake.head_x_cor() <= -385 or snake.head_y_cor() >= 325 or snake.head_y_cor() <= -325:
+        scoreboard.game_over()
+        game_is_on = False
+
+    # detect if hit himself
+    for i in range(len(snake.snake)-1):
+        if snake.head_x_cor() == snake.snake[i].xcor():
+            print("Bruh")
+
 screen.exitonclick()
