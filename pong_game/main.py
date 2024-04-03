0920866b77b9fb6ea8171e4533242ce7323117c7
Hien Tran <79175744+hientran-sudo@users.noreply.github.com>
2024-04-03 17:25:40 -0400

    Create main.py

---
+++ pong_game/main.py
@@ -0,0 +1,36 @@
+from turtle import Screen
+from paddle import Paddle
+from ball import Ball
+import time
+
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
+screen.title("Pong")
+screen.tracer(0)
+
+r_paddle = Paddle(350,0)
+l_paddle = Paddle(-350,0)
+ball = Ball()
+
+screen.listen()
+screen.onkey(fun=r_paddle.go_up, key="Up")
+screen.onkey(fun=r_paddle.go_down, key="Down")
+screen.onkey(fun=l_paddle.go_up, key="w")
+screen.onkey(fun=l_paddle.go_down, key="s")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    ball.move()
+
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce()
+
+    # detect collision with right paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() :
+        ball.bounce()
+
+
+screen.exitonclick()
