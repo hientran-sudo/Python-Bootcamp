988a4251e79ba8df3e12656af1ca3db9634e7b8e
Hien Tran <79175744+hientran-sudo@users.noreply.github.com>
2024-04-11 12:38:36 -0400

    Update main.py

--- crossing_stress_game/main.py
+++ crossing_stress_game/main.py
@@ -1,7 +1,7 @@
 from turtle import Screen
 from tim import Tim
 from car import Car
-#from scoreboard import Scoreboard
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -11,6 +11,8 @@ screen.tracer(0)
 
 tim = Tim()
 car = Car()
+score = Scoreboard()
+score.level()
 
 screen.listen()
 screen.onkey(fun=tim.go_up, key="Up")
@@ -21,5 +23,16 @@ while game_is_on:
     time.sleep(0.1)
     car.generate_car()
     car.move()
+
+    #detect collision
+    for i in car.set:
+        if tim.distance(i) < 20:
+            score.game_over()
+            game_is_on = False
+    
+    if tim.ycor() == 300:
+        score.level_up()
+        tim.goto(0, -280)
+        car.speed_up()
       
 screen.exitonclick()
