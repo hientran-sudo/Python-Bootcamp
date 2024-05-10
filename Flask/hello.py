1bc209f29d91592cfe9a9d69e2ec42c85822eccc
Hien Tran <79175744+hientran-sudo@users.noreply.github.com>
2024-05-10 10:00:32 -0400

    Update hello.py

--- Flask/hello.py
+++ Flask/hello.py
@@ -1,13 +1,19 @@
+https://code.visualstudio.com/docs/python/tutorial-flask
+
 from flask import Flask
 app = Flask(__name__)
 
+def bye():
+  return "Bye!"
+
 @app.route('/')
 def hello_world():
-  return 'Hello World!'
+  return '<h1 style="text-align: center">Hello World!</h1>' \
+         '<p> Paragraph </p>' 
 
 @app.route('/username/<name>/<int:number>')
 def great(name):
-  return 'Hello {name}. You're {number} yrs old!'
+  return "Hello {name}. You're {number} yrs old!"
 
 
 # tell ternminal by exporting the FLASK_APP environment variable:
